d869c01cfb6767363d553eff47aba210c05f6f9e
emigdioz <emigdioz@gmail.com>
2015-10-16 15:18:17 -0700

    Matrix handling sample

--- matrix.h
+++ matrix.h
@@ -1,5 +1,16 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+class Matrix {
+    int* array;
+    int m_width;
+public:
+    Matrix( int w, int h ) : m_width( w ), array( new int[ w * h ] ) {}
+    ~Matrix() { delete[] array; }
+    int at( int x, int y ) const { return array[ index( x, y ) ]; }
+protected:
+    int index( int x, int y ) const { return x + m_width * y; }
+};
+
 #endif // MATRIX_H
 
