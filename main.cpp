f42837e245ae8b57894d3ca0181545d62e20daeb
emigdioz <emigdioz@gmail.com>
2015-10-16 15:15:44 -0700

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-class taps{
-public:
-    taps();
-    void jajaja(){};
-};
-
-taps::taps()
-{
-
-}
-
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow window;
-    //window.resize(640, 400);
     window.show();
-
-    taps *t=new taps();
-    //hola();
-    //start_main(argc,argv);
-
     return app.exec();
 }
