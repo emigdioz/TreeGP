40343ffc7bd532963dc6755fec5ba9a8824cce78
emigdioz <emigdioz@gmail.com>
2015-10-21 16:32:50 -0700

    New options for selectable functions

--- optionsdialog.cpp
+++ optionsdialog.cpp
@@ -6,6 +6,86 @@ OptionsDialog::OptionsDialog(QWidget *parent) :
     ui(new Ui::OptionsDialog)
 {
     ui->setupUi(this);
+    model = new QStandardItemModel(this);
+    ui->functionsView->setModel(model);
+
+    QStringList list;
+    QStandardItem *newItem = 0;
+    list.append("Enabled");
+    list.append("Description");
+    model->setHorizontalHeaderLabels(list);
+
+    newItem = new QStandardItem("+");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Add");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
+
+    newItem = new QStandardItem("-");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Substract");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
+
+    newItem = new QStandardItem("*");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Multiply");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
+
+    newItem = new QStandardItem("/");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Divide");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
+
+    newItem = new QStandardItem("Sin");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Sine");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
+
+    newItem = new QStandardItem("Cos");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Cosine");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
+
+    newItem = new QStandardItem("Log");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Logarithm");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
+
+    newItem = new QStandardItem("Exp");
+    newItem->setCheckable(true);
+    newItem->setCheckState(Qt::Checked);
+    standardItemList.append(newItem);
+    newItem = new QStandardItem("Exponential");
+    standardItemList.append(newItem);
+    model->appendRow(standardItemList);
+    standardItemList.clear();
 
 }
 
@@ -41,6 +121,7 @@ void OptionsDialog::read_values(QString &output,int choice)
 void OptionsDialog::accept()
 {
     Options GPparam;
+
     GPparam.ngen = ui->spinBox->value();
     GPparam.popsize = ui->lineEdit->text().toInt();
     GPparam.tournamentsize = ui->lineEdit_2->text().toInt();
@@ -52,6 +133,11 @@ void OptionsDialog::accept()
     GPparam.mutationp = ui->lineEdit_8->text().toFloat();
     GPparam.mutationmaxr = ui->lineEdit_9->text().toInt();
     GPparam.randomseed = ui->lineEdit_10->text().toLong();
+    QModelIndex index;
+    for(int i=0;i<8;i++) {
+      index = ui->functionsView->model()->index(i,0, QModelIndex());
+      if(index.data(Qt::CheckStateRole) == Qt::Checked) GPparam.functionselection.push_back(true); else GPparam.functionselection.push_back(false);
+    }
     emit send_data(GPparam);
     hide();
 }
