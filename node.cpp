a028987054888e047d84ff863d44b5e37e5dc2c4
emigdioz <emigdioz@gmail.com>
2015-10-23 16:54:21 -0700

    Clean up

--- node.cpp
+++ node.cpp
@@ -158,24 +158,22 @@ QPainterPath Node::shape() const
 //! [10]
 void Node::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *)
 {
-    painter->setPen(Qt::NoPen);
-    painter->setBrush(Qt::darkGray);
-    painter->drawEllipse(-7, -7, 20, 20);
-
-    QRadialGradient gradient(-3, -3, 10);
     if (option->state & QStyle::State_Sunken) {
-        gradient.setCenter(3, 3);
-        gradient.setFocalPoint(3, 3);
-        gradient.setColorAt(1, QColor(Qt::yellow).light(120));
-        gradient.setColorAt(0, QColor(Qt::darkYellow).light(120));
+      painter->setBrush(QColor(255, 105, 156, 255));
     } else {
-        gradient.setColorAt(0, Qt::yellow);
-        gradient.setColorAt(1, Qt::darkYellow);
+      painter->setBrush(QColor(0, 173, 250, 255));
     }
-    painter->setBrush(gradient);
-
-    painter->setPen(QPen(Qt::black, 0));
+    painter->setPen(Qt::NoPen);
     painter->drawEllipse(-10, -10, 20, 20);
+
+//    QRectF textRect(-10, -10, 20, 20);
+//    QFont font = painter->font();
+//    font.setBold(false);
+//    font.setPointSize(14);
+//    painter->setFont(font);
+//    painter->setPen(Qt::white);
+//    painter->drawText(textRect, Qt::AlignCenter,GraphWidget::tr("+"));
+
 }
 //! [10]
 
