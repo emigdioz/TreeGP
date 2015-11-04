3112b893af86e23dc4b51236d719e7bda4103218
emigdioz <emigdioz@gmail.com>
2015-11-03 19:05:28 -0800

    Fixes

--- graphwidget.h
+++ graphwidget.h
@@ -42,6 +42,7 @@
 #define GRAPHWIDGET_H
 
 #include <QGraphicsView>
+#include <QScrollBar>
 
 class Node;
 
@@ -69,15 +70,15 @@ protected:
     void drawBackground(QPainter *painter, const QRectF &rect) Q_DECL_OVERRIDE;    
 
     void scaleView(qreal scaleFactor);
-//    virtual void mouseMoveEvent(QMouseEvent *event);
-//    virtual void mousePressEvent(QMouseEvent *event);
-//    virtual void mouseReleaseEvent(QMouseEvent *event);
     //Holds the current centerpoint for the view, used for panning and zooming
     QPointF CurrentCenterPoint;
     //From panning the view
     QPoint LastPanPoint;
 
 private:
+    virtual void mouseMoveEvent(QMouseEvent *event);
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseReleaseEvent(QMouseEvent *event);
     int timerId;
     Node *centerNode;
     bool _pan;
